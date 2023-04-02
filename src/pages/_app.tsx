fc6e77cc83b57e88e39f46933a5f03cb5f502bc3
Leonardo <leoleo1@hotmail.com>
2023-04-02 19:25:50 -0300

    feat: migrate to tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,6 @@
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
-import { theme } from '@styles/theme'
-import GlobalStyles from '@styles/globalStyles'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import { Flex } from '@styles/commonStyles'
 import { Layout } from '@components/layout/layout'
 
 export default function Home() {
   return (
     <>
-      <Layout title="Codemunity" description="Página principal">
-        <Flex>Hello World!</Flex>
-      </Layout>
+      {/* <Layout title="Codemunity" description="Página principal"> */}
+      <div className="text-3xl font-bold underline">Hello World!</div>
+      {/* </Layout> */}
     </>
   )
 }
