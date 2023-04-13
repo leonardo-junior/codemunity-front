491888eba3cc6b46678a58c6e2ee58c007ff1df7
leonardo-junior <leoleo1@hotmail.com>
2023-04-13 20:53:27 -0300

    feat: update types

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,4 +1,18 @@
 export type Course = {
   id: number
   name: string
+  courseSections: CourseSection[]
+}
+
+export type CourseSection = {
+  id: number
+  name: string
+  courseId: number
+  classes: Class[]
+}
+
+export type Class = {
+  id: number
+  name: string
+  url: string
 }
